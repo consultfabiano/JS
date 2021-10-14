9082b18c3ad4d880ace9f41f6c3aa53551f8d142
Fabiano Garcia <fabianojp06@gmail.com>
2021-10-14 15:58:11 -0300

    CRUD realizado

--- projeto/models/atendimentos.js
+++ projeto/models/atendimentos.js
@@ -36,7 +36,7 @@ class Atendimento {
         if(erro) {
                 res.status(400).json(erro)
             } else {
-                res.status(201).json(resultado)
+                res.status(201).json(atendimento)
             }
         })
 
@@ -44,5 +44,59 @@ class Atendimento {
 
         
     }
+
+    lista(res) {
+        const sql = 'SELECT * FROM Atendimentos'
+
+        conexao.query(sql, (erro, resultado) => {
+            if(erro){
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json(resultado)
+            }
+        })
+    }
+
+    buscaPorId(id, res) {
+        const sql = `SELECT * FROM Atendimentos WHERE id=${id}`
+
+        conexao.query(sql, (erro, resultado) => {
+            const atendimento = resultado[0]
+
+            if(erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json(atendimento)
+            }
+        })
+    }
+
+    altera(id, valores, res) {
+        if(valores.data) {
+            valores.data = moment(valores.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+        }
+        const sql = 'UPDATE Atendimentos SET ? WHERE id=?'
+
+        conexao.query(sql, [valores, id], (erro, resulado) =>{
+            if(erro) {
+                res.status(400).json(erro)
+            }else {
+                res.status(200).json({...valores, id})
+            }
+        })
+    }
+
+    deleta(id, res) {
+        const sql = 'DELETE FROM Atendimentos WHERE id=?'
+
+        conexao.query(sql, id, (erro, resultado) => {
+            if(erro){
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json({id})
+            }
+        })
+    }
+
 }
 module.exports = new Atendimento
